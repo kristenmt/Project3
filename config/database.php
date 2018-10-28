09fd427e9f700e360711613a953a1a98891bd9d2
kristenmt <kc7795@hotmail.com>
2018-10-28 16:40:35 -0400

    code fixes for deployment

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$dbUrl = parse_url(env("JAWSDB_URL"));
 return [
 
     /*
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '8889'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'final_db'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
